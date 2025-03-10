0016ec46e556ab7e18554d4c075b46bde178cdce
Danielle Stanley <STNDAN021@myuct.ac.za>
2025-03-10 16:31:44 +0200

    Changing python script to use .csv file generated from .dat file

--- P1/Load_CTD_Data_Frame.py
+++ P1/Load_CTD_Data_Frame.py
@@ -1,7 +1,7 @@
 import pandas as pd 
 
 
-ctd_file = pd.read_csv("20081129_0652_CTDDATA.dat", names=['Date', 'Time', 'Depth (m)', 'Temperature (celsius)', 'Salinity (psu)'], delimiter="\t")
+ctd_file = pd.read_csv("../../Desktop/Assignments/BIO5012W/SCDM/P1_Assignment/20081129_0652_CTDDATA.csv")
 
 ctd_dataframe = pd.DataFrame(ctd_file)
 
